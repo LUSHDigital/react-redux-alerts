70c379eda22e2f4caebb6b0be93ab1a42c294298
Rich Comber <rich@elementalwebdesign.co.uk>
2017-12-13 21:10:57 +0000

    Getting basics working for dismissing & auto timeout

--- src/components/alert.js
+++ src/components/alert.js
@@ -39,7 +39,7 @@ const AlertWrapper = styled.div`
     left: 0;
     top: 0;
     position: absolute;
-    height: 2px;
+    height: ${ props => props.dismissible ? '2px' : '0px' };
     background: rgba(255,255,255,0.3);
     webkit-animation-fill-mode: both;
     animation-fill-mode: both;
@@ -69,20 +69,42 @@ class Alert extends Component {
 
   componentDidMount() {
     const { alert } = this.props;
-    this.props.actions.setAlertClear(alert.id, alert.alert.time);
+    if (this.isDismissible() && this.hasAutoTimeout()) {
+      this.props.actions.setAlertClear(alert.id, alert.alert.time);
+    }
+  }
+
+  isDismissible() {
+    const { alert } = this.props;
+    return typeof alert.alert.dismissible === 'undefined' || alert.alert.dismissible;
+  }
+
+  hasAutoTimeout() {
+    const { alert } = this.props;
+    return typeof alert.alert.autoTimeout === 'undefined' || alert.alert.autoTimeout;
+  }
+
+  renderClose() {
+    const { alert } = this.props;
+    if (this.isDismissible()) {
+      return(
+        <Close className='alert__close' onClick={ () => this.props.actions.clearAlert(this.props.alert.id) } role='button' tabIndex='-1'>
+        { this.props.closeIcon }
+      </Close>
+      )
+    }
+    return null;
   }
 
   render() {
     const { alert, colours } = this.props;
     if (alert) {
       return (
-          <AlertWrapper className={ `alert ${ alert.alert.type }` } colours={ colours } time={ alert.alert.time }>
+          <AlertWrapper className={ `alert ${ alert.alert.type }` } colours={ colours } time={ alert.alert.time } dismissible={ this.isDismissible() && this.hasAutoTimeout() }>
             <div className='container'>
               <div className='cell'>
                 { alert.alert.message}
-                <Close className='alert__close' onClick={ () => this.props.actions.clearAlert(this.props.alert.id) } role='button' tabIndex='-1'>
-                  { this.props.closeIcon }
-                </Close>
+                { this.renderClose() }
               </div>
             </div>
           </AlertWrapper>
